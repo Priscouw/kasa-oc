c301a598bf54e412f05ded806c61b7f9df471ac7
Priscouw <priscouw@gmail.com>
2025-02-12 11:19:26 +0100

    <feat> create Erreur page

---
+++ src/pages/Erreur/Erreur.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Erreur = () => {
+  return (
+    <div>
+      <div className="error404">
+        <h1 className="error404-title">404</h1>
+        <p className="error404-text">
+          Oups! La page que <br /> vous demandez n&apos;existe pas.
+        </p>
+        <Link to={`/`} className="error404-link">
+          Retourner sur la page d’accueil
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Erreur;
