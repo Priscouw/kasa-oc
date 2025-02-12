1adbaa55639957c2eb4551fa5c14e5d9c3cca454
Priscouw <priscouw@gmail.com>
2025-02-12 11:34:04 +0100

    <refactor> remove div, add main tag

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ import Erreur from "./pages/Erreur/Erreur";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
+    <BrowserRouter>
+      <Header />
+      <main>
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/apropos" element={<Apropos />} />
           <Route path="/logement/:id" element={<Logement />} />
           <Route path="*" element={<Erreur />} />
         </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
